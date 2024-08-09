79075d3505f90304f6279d9bce86a2d94a9c437f
moji <mojtaba@klaie@gmail.com>
2024-08-09 21:23:07 +0330

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const box = document.getElementById("box");
+const divBox = document.getElementById("div_box");
+
+const box2 = document.getElementById("box2");
+const divBox2 = document.getElementById("div_box2");
+
+const box3 = document.getElementById("box3");
+const divBox3 = document.getElementById("div_box3");
+
+function mouseDown() {
+    box.style.cssText="transform: translateY(-30px); background-color: 5998ee;color:#145e14";
+    divBox.style.zIndex="1";
+}
+
+function mouseBlur() {
+    box.style.cssText="transform: translateY(0); background-color: #5998ee;color:#f0f8ff";
+}
+
+function mouseDown2() {
+    box2.style.cssText="transform: translateY(-30px);  background-color: 5998ee;color:#145e14";
+    divBox2.style.zIndex="1";
+}
+
+function mouseBlur2() {
+    box2.style.cssText="transform: translateY(0); background-color: #5998ee;color:#f0f8ff";
+}
+
+function mouseDown3() {
+    box3.style.cssText="transform: translateY(-30px);background-color: 5998ee;color:#145e14";
+    divBox3.style.zIndex="1";
+}
+
+function mouseBlur3() {
+    box3.style.cssText="transform: translateY(0); background-color: #5998ee;color:#f0f8ff";
+}
